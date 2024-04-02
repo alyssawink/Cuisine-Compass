a294f452c6efbabfd25ae42da29979b089ff0f83
97rhoover <97rhoover@gmail.com>
2024-04-01 21:45:00 -0400

    Added JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,47 +1,18 @@
 const apiKEY = 'zWPt5cfKm9OawNFe1JwFlPyWec-8IkXkdiC6PZj01sJD5xKQ0LgM-rceW9RmJeWC0g58qltGLn09IvYzcaDSE9kUgUcmc9C257rqsj8Qk38CinS-ajHme0nRh3kDZnYx'
 const userFormEl = document.querySelector('#user-form');
-const locInputEL = document.querySelector('#search-term-input');
 
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
 
-const formSubmitHandler = function (event) {
-    event.preventDefault();
+  const cuisineInput = document.querySelector('#search-term-input').value;
+  const locationInput = document.querySelector('#search-term-location').vaule;
 
-    const loc = locInputEL.value.trim();
+  if(!cuisineInput, locationInput) {
+    console.error('Please enter Cuisine and Location.')
+    return;
+  }
+  const queryString = `./landing.html?q=${cuisineInput}&format=${locationInput}`;
 
-    if (loc) {
-        getUserLoc(loc);
-
-        locContainerEl.textContent = '';
-        locInputEL.value = '';
-    } else {
-      alert('Please enter a valid location.');
-
-    }
-};
-
-const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer zWPt5cfKm9OawNFe1JwFlPyWec-8IkXkdiC6PZj01sJD5xKQ0LgM-rceW9RmJeWC0g58qltGLn09IvYzcaDSE9kUgUcmc9C257rqsj8Qk38CinS-ajHme0nRh3kDZnYx'
-    }
-  };
-  
-  const getUserSearch = function (location) {
-    
-    const apiURL = 'https://api.yelp.com/v3/businesses/search?sort_by=best_match&limit=20&location=${location}';
-  
-  fetch(apiURL, options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+  location.assign(queryString);
 }
-
-
-
-
-
-
-
-
-userFormEl.addEventListener('submit', formSubmitHandler);
+userFormEl.addEventListener('submit', handleSearchFormSubmit);
